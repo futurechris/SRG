053da65ae6dd659618d1400b52efc1437550e151
Chris Mills-Price <millspch@gmail.com>
2015-09-29 16:46:00 -0700

    Use values from settings.json to initialize twitter feed on server start

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,3 +1,19 @@
 Meteor.startup(function () {
     // code to run on server at startup
+    initializeTwitterFeed();
 });
+
+function initializeTwitterFeed()
+{
+	if(typeof(Meteor.settings.twit_keys) !== "undefined")
+	{
+		var Twit = Meteor.npmRequire('twit');
+
+		Meteor.Twit = new Twit({
+		    consumer_key:         Meteor.settings.twit_keys.tw_consumer_key,
+		    consumer_secret:      Meteor.settings.twit_keys.tw_consumer_secret,
+		    access_token:         Meteor.settings.twit_keys.tw_access_token,
+		    access_token_secret:  Meteor.settings.twit_keys.tw_access_token_secret
+		});
+	}
+}
